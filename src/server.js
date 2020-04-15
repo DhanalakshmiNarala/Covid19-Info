7992ec6e1b9667292022e8a3da000681e41a2ee5
Dhanalakshmi Narala <dhanalakshmi.narala@gmail.com>
2020-04-15 21:38:07 +0530

    Add initial server setup

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const { resolve } = require('path');
+require('dotenv').config({ path: resolve(__dirname, '../config/dev.env') });
+
+const port = process.env.SERVER_PORT || 5000;
+const app = require('./app');
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
