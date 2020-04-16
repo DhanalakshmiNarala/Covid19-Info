e2faf4502a95412008396b47d62e49291b720b5c
Dhanalakshmi Narala <dhanalakshmi.narala@gmail.com>
2020-04-16 21:29:14 +0530

    Add sequelize-automate config file

---
+++ sequelize-automate.config.js
@@ -0,0 +1,17 @@
+require('dotenv').config({ path: './config/dev.env' });
+
+module.exports = {
+  dbOptions: {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    dialect: process.env.DB_TYPE,
+    host: process.env.HOST,
+    port: process.env.DB_PORT,
+    logging: false,
+  },
+  options: {
+    type: 'js',
+    dir: 'src/models',
+  },
+};
