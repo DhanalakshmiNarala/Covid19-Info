8210a5069e2da9666e0e559843ebcf89ff64d2c2
Dhanalakshmi Narala <dhanalakshmi.narala@gmail.com>
2020-04-17 13:31:48 +0530

    Add morgan and CORS errors handling

--- src/app.js
+++ src/app.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
-app.use('/', (req, res, next) => {
-  res.send('Everything working fine');
+app.use(morgan('dev'));
+app.use(express.json());
+
+//Handling CORS errors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'POST, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    error: {
+      message: 'Route not found',
+    },
+  });
 });
 
 module.exports = app;
