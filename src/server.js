3424affd48c7c19868a943d904d0be1fb1029845
Dhanalakshmi Narala <dhanalakshmi.narala@gmail.com>
2020-04-20 07:27:35 +0530

    Change server setup for testing

--- src/server.js
+++ src/server.js
@@ -4,8 +4,10 @@ require('dotenv').config({ path: resolve(__dirname, '../config/dev.env') });
 const port = process.env.SERVER_PORT || 5000;
 const app = require('./app');
 
-app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
-});
+if (!module.parent) {
+  app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+  });
+}
 
 module.exports = app;
