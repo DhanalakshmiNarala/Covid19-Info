a15e0bcb7d2956d2edc0962f3dd56345c67a816f
Dhanalakshmi Narala <dhanalakshmi.narala@gmail.com>
2020-04-21 11:16:32 +0530

    Refactor server.js code environmental variables

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ require('dotenv').config({ path: resolve(__dirname, '../config/dev.env') });
 const port = process.env.SERVER_PORT || 5000;
 const app = require('./app');
 
-if (!module.parent) {
+if (process.env.NODE_ENV !== 'testing') {
   app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
   });
