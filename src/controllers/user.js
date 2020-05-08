4a32b2b5facd83a713aef4cb445b80e2e2211cb5
Dhanalakshmi Narala <dhanalaskhmi.narala@gmail.com>
2020-05-08 15:36:24 +0530

    Refactor /signUp routes

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,5 @@
-const { insertUserInDB } = require('../services/user');
-const { successResponse, failureResponse } = require('../utils/response');
+const { insertUserInDB } = require("../services/user");
+const { successResponse, failureResponse } = require("../utils/response");
 
 const addUser = async (req, res, next) => {
   try {
@@ -9,24 +9,24 @@ const addUser = async (req, res, next) => {
       confirm_password: req.body.confirm_password,
       name: req.body.name,
       email: req.body.email,
-      is_admin: req.body.is_admin || false, // fixme: req.is_admin should'nt be allowed
+      is_admin: false,
     };
     if (user.password === user.confirm_password) {
-      const userRecord = await insertUserInDB(user);
-      if (userRecord) {
+      const dbUser = await insertUserInDB(user);
+      if (dbUser) {
         return successResponse(res, {
           status: 200,
-          message: 'User successfully signed up',
+          message: "User successfully signed up",
         });
       }
       return failureResponse(res, {
         status: 400,
-        message: 'User sign up failed',
+        message: "User sign up failed",
       });
     } else {
       return failureResponse(res, {
         status: 400,
-        message: 'Password and confirm password are not equal',
+        message: "Password and confirm password are not equal",
       });
     }
   } catch (error) {
--- src/server.js
+++ src/server.js
@@ -7,5 +7,3 @@ const app = require("./app");
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
-
-module.exports = app;
