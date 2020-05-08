1d63b68f907a1f8865b930a3899e75265e98798c
Dhanalakshmi Narala <dhanalaskhmi.narala@gmail.com>
2020-05-08 12:31:51 +0530

    Add sequelize connection pooling

--- src/utils/sequelize-connect.js
+++ src/utils/sequelize-connect.js
@@ -1,7 +1,7 @@
-const { resolve } = require('path');
-require('dotenv').config({ path: resolve(__dirname, '../../config/dev.env') });
+const { resolve } = require("path");
+require("dotenv").config({ path: resolve(__dirname, "../../config/dev.env") });
 
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -11,6 +11,12 @@ const sequelize = new Sequelize(
     dialect: process.env.DB_TYPE,
     define: { timestamps: false },
     logging: false,
+    pool: {
+      max: 95,
+      min: 0,
+      idle: 20000,
+      acquire: 20000,
+    },
   }
 );
 
